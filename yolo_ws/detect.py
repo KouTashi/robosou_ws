da925dd68737b294525d6864487f50f92e82fa64
K.Tashiro <tashikou1682@gmail.com>
2025-10-31 22:37:57 +0900

    CPU,GPUの比較

--- yolo_ws/detect.py
+++ yolo_ws/detect.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO
 
-model = YOLO("/home/dev/robosou/yolo_ws/best.pt")  # fast > n s m l x > slow
+model = YOLO("/home/dev/robosou_ws/src/yolo_ws/best_m.pt")  # fast > n s m l x > slow
 
 results = model.predict(
-    source="/home/dev/robosou/yolo_ws/sample/Screencast from 2025-10-09 11-17-38.webm",
+    source="2",
     show=True,
 )
--- yolo_ws/detect_intelgpu.py
+++ yolo_ws/detect_intelgpu.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load a YOLOv8n PyTorch model
-model = YOLO("/home/dev/robosou_ws/src/yolo_ws/best.pt")  # fast > n s m l x > slow
+model = YOLO("/home/dev/robosou_ws/src/yolo_ws/best_m.pt")  # fast > n s m l x > slow
 
 # Export the model
 model.export(format="openvino")  # creates 'yolov8n_openvino_model/'
@@ -10,7 +10,7 @@ model.export(format="openvino")  # creates 'yolov8n_openvino_model/'
 ov_model = YOLO("/home/dev/robosou_ws/src/yolo_ws/best_openvino_model/")
 
 results = ov_model.predict(
-    source="0",
+    source="2",
     conf=0.5,
     show=True,
 )
